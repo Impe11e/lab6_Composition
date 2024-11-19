74f529ae6e40bc0cb2fb4b1b51ef0e3070fe5b8f
ariia horunzha <1mpellee@gmail.com>
2024-11-19 11:51:15 +0200

    first commit

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,20 @@
 'use strict';
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+    for (const f of fns) {
+      if (typeof f !== 'function') {
+        throw new Error('All arguments should be functions!')
+      }
+    }
+    return (x) => {
+      let res = x;
+      for (let i = 0; i < fns.length; i++) {
+        res = fns[i](res);
+      }
+      return res;
+    }
+  }
+ 
+  // or i can just write this: return (x) => fns.reduce((v, f) => f(v), x);
 
 module.exports = { pipe };
